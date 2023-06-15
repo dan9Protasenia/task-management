15054dee2d7bb0afcb70b841c2491625230b856c
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-15 18:39:11 +0300

    delete
    delete

--- job_service/bd.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-bd = SQLAlchemy()
-
-class Job(bd.Model):
-    id = bd.Column(bd.Integer, primary_key=True)
-    name_position = bd.Column(bd.String(50), nullable=False)
-    tariff_rate = bd.Column(bd.Integer, nullable=False)
