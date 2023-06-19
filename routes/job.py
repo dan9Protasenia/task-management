b6907c108d5ce2165c5312dead5e4b8bda9983d3
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-19 16:24:03 +0300

    Update job.py

--- routes/job.py
+++ routes/job.py
@@ -1,56 +1,33 @@
 from flask import Blueprint, request, jsonify
-from service.job_repository import JobRepository, Job
+from service.job_service import get_all_jobs, create_job, update_job, delete_job
+import logging
 
 job = Blueprint('job', __name__)
-repository = JobRepository()
 
-@job.route('/job', methods=['GET'])
-def jobs():
-    jobs = repository.get_all()
-    job_list = []
-    for job in jobs:
-        job_data = {
-            'name': job.name,
-            'tariff_rate': job.tariff_rate
-        }
-        job_list.append(job_data)
-    return jsonify(jobs=job_list)
+@job.route('/jobs', methods=['GET'])
+def get_jobs():
+    jobs = get_all_jobs()
+    return jsonify(jobs=jobs)
 
+@job.after_request
+def after_request(response):
+    log_message = f'{request.method} {request.path} - {response.status_code}'
+    logging.info(log_message)
+    return response
 
 @job.route('/create_job', methods=['POST'])
-def create_job():
+def create_job_route():
     data = request.get_json()
-    name_position = data['name_position']
-    tariff_rate = data['tariff_rate']
-
-    job = Job(
-        name_position=name_position,
-        tariff_rate=tariff_rate
-    )
-
-    job_repository.create(job)
-
+    create_job(data)
     return jsonify(message='Job created successfully')
 
-
 @job.route('/edit_job/<int:job_id>', methods=['PUT'])
-def edit_job(job_id):
-    job = repository.get_by_id(job_id)
-
+def edit_job_route(job_id):
     data = request.get_json()
-    job.name_position = data['name_position']
-    job.tariff_rate = data['tariff_rate']
-
-    repository.update(job)
-
+    update_job(job_id, data)
     return jsonify(message='Job updated successfully')
 
-
 @job.route('/delete_job/<int:job_id>', methods=['DELETE'])
-def delete_job(job_id):
-    job = repository.get_by_id(job_id)
-    repository.delete(job)
-
+def delete_job_route(job_id):
+    delete_job(job_id)
     return jsonify(message='Job deleted successfully')
-
-
