9cc45ad04ed6d3282b809d74c90d074a4b1ff707
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-21 14:09:04 +0300

    added constants

---
+++ tests/constants.py
@@ -0,0 +1,84 @@
+# statuses
+STATUS_IN_PROGRESS = 'In Progress'
+STATUS_COMPLETED = 'Completed'
+
+# project names
+PROJECT_NEW = 'New Project'
+PROJECT_TEST = 'Test Project'
+PROJECT_UPDATED = 'Updated Project'
+
+# project short names
+SHORT_NAME_NEW = 'NP'
+SHORT_NAME_TEST = 'TP'
+SHORT_NAME_UPDATED = 'UP'
+
+# project descriptions
+DESCRIPTION_TEST = 'Test project'
+DESCRIPTION_UPDATED = 'Updated test project'
+DESCRIPTION_TEST_PROJECT = 'Test project description'
+
+# project date
+START_DATE_TEST = '2023-01-01'
+PLANNED_END_DATE = '2023-02-01'
+
+# cost project
+COST_1000 = 1000.0
+COST_1500 = 1500.0
+COST_2000 = 2000.0
+COST_100 = 100.0
+# task names
+TASK_NEW = 'New Task'
+TASK_UPDATED = 'Updated Task'
+TASK1 = 'Task 1'
+TASK2 = 'Task 2'
+TASK3 = 'Task 3'
+
+# task priority
+PRIORITY_HIGH = 'High'
+PRIORITY_MEDIUM = 'Medium'
+PRIORITY_LOW = 'Low'
+
+# names jobs
+NAME_JOB_1 = 'Job 1'
+NAME_JOB_2 = 'Job 2'
+NAME_JOB_3 = 'Job 3'
+NAME_JOB_4 = 'Job 4'
+NAME_NEW_JOB = 'New Job'
+NAME_UPDATED_JOB = 'Updated Job'
+NAME_TEST_JOB = 'Test Job'
+
+# cost jobs
+TARIFF_RATE_10 = 10.0
+TARIFF_RATE_20 = 20.0
+TARIFF_RATE_9999999999990 = 9999999999990.0
+TARIFF_RATE_0 = 0.0
+TARIFF_RATE_15 = 15.0
+
+# names employees
+LAST_NAME_1 = 'Last 1'
+LAST_NAME_2 = 'Last 2'
+LAST_NAME_3 = 'Last 3'
+LAST_NAME_4 = 'Last 4'
+LAST_NAME_TEST = 'Test Employee'
+LAST_NAME_UPDATED = 'Updated Last'
+FIRST_NAME_1 = 'First 1'
+FIRST_NAME_2 = 'First 2'
+FIRST_NAME_3 = 'First 3'
+FIRST_NAME_4 = 'First 4'
+FIRST_NAME_JOHN = 'John'
+FIRST_NAME_UPDATED = 'Updated First'
+MIDDLE_NAME_1 = 'Middle 1'
+MIDDLE_NAME_2 = 'Middle 2'
+MIDDLE_NAME_3 = 'Middle 3'
+MIDDLE_NAME_4 = 'Middle 4'
+MIDDLE_NAME_DOE = 'Doe'
+MIDDLE_NAME_UPDATED = 'Updated Middle'
+
+# position employees
+POSITION_1 = 'Position 1'
+POSITION_2 = 'Position 2'
+POSITION_3 = 'Position 3'
+POSITION_4 = 'Position 4'
+POSITION_ENGINEER = 'Engineer'
+POSITION_UPDATED = 'Updated Position'
+
