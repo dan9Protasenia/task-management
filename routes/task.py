4e08f624bf8cd4ad97d96efb2028c7f78e1ff4eb
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-26 17:32:43 +0300

    added filter for task

--- routes/task.py
+++ routes/task.py
@@ -10,7 +10,49 @@ repository = TaskRepository()
 
 @task.route('/tasks/<int:project_id>', methods=['GET'])
 def get_tasks(project_id):
-    tasks = Task.query.filter_by(project_id=project_id).all()
+    name_filter = request.args.get('name')
+    start_date_from_filter = request.args.get('start_date_from')
+    start_date_to_filter = request.args.get('start_date_to')
+    end_date_from_filter = request.args.get('end_date_from')
+    end_date_to_filter = request.args.get('end_date_to')
+    overdue_filter = request.args.get('overdue')
+    cost_from_filter = request.args.get('cost_from')
+    cost_to_filter = request.args.get('cost_to')
+
+    tasks_query = Task.query.filter_by(project_id=project_id)
+
+    if name_filter:
+        tasks_query = tasks_query.filter(Task.name.ilike(f'%{name_filter}%'))
+
+    if start_date_from_filter:
+        start_date_from = date.fromisoformat(start_date_from_filter)
+        tasks_query = tasks_query.filter(Task.start_date >= start_date_from)
+
+    if start_date_to_filter:
+        start_date_to = date.fromisoformat(start_date_to_filter)
+        tasks_query = tasks_query.filter(Task.start_date <= start_date_to)
+
+    if end_date_from_filter:
+        end_date_from = date.fromisoformat(end_date_from_filter)
+        tasks_query = tasks_query.filter(Task.planned_end_date >= end_date_from)
+
+    if end_date_to_filter:
+        end_date_to = date.fromisoformat(end_date_to_filter)
+        tasks_query = tasks_query.filter(Task.planned_end_date <= end_date_to)
+
+    if overdue_filter == 'true':
+        tasks_query = tasks_query.filter(Task.planned_end_date < date.today())
+
+    if cost_from_filter:
+        cost_from = float(cost_from_filter)
+        tasks_query = tasks_query.filter(Task.cost >= cost_from)
+
+    if cost_to_filter:
+        cost_to = float(cost_to_filter)
+        tasks_query = tasks_query.filter(Task.cost <= cost_to)
+
+    tasks = tasks_query.all()
+
     task_list = []
     for task in tasks:
         task_data = {
@@ -23,6 +65,7 @@ def get_tasks(project_id):
             'status': task.status
         }
         task_list.append(task_data)
+
     return jsonify(tasks=task_list)
 
 
