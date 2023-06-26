cabc11ea29ec0ab9f50e3e42c200f60118691646
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-26 18:05:21 +0300

    added filter for employee

--- service/employee_service.py
+++ service/employee_service.py
@@ -3,18 +3,34 @@ from repository.employee_repository import EmployeeRepository, Employee
 repository = EmployeeRepository()
 
 
-def get_all_employees():
+def get_all_employees(search_query=None):
     employees = repository.get_all()
-    employee_list = []
-    for employee in employees:
-        employee_data = {
-            'last_name': employee.last_name,
-            'first_name': employee.first_name,
-            'middle_name': employee.middle_name,
-            'position': employee.position
-        }
-        employee_list.append(employee_data)
-    return employee_list
+
+    if search_query:
+        filtered_employees = []
+        for employee in employees:
+            if search_query.lower() in employee.last_name.lower() or \
+                    search_query.lower() in employee.first_name.lower() or \
+                    search_query.lower() in employee.middle_name.lower():
+                employee_data = {
+                    'last_name': employee.last_name,
+                    'first_name': employee.first_name,
+                    'middle_name': employee.middle_name,
+                    'position': employee.position
+                }
+                filtered_employees.append(employee_data)
+        return filtered_employees
+    else:
+        employee_list = []
+        for employee in employees:
+            employee_data = {
+                'last_name': employee.last_name,
+                'first_name': employee.first_name,
+                'middle_name': employee.middle_name,
+                'position': employee.position
+            }
+            employee_list.append(employee_data)
+        return employee_list
 
 
 def create_employee(data):
@@ -33,6 +49,8 @@ def create_employee(data):
     repository.create(employee)
 
     return employee
+
+
 def update_employee(employee_id, data):
     employee = repository.get_by_id(employee_id)
     employee.last_name = data['last_name']
@@ -42,6 +60,8 @@ def update_employee(employee_id, data):
     repository.update(employee)
 
     return employee
+
+
 def delete_employee(employee_id):
     employee = repository.get_by_id(employee_id)
     repository.delete(employee)
