70a579202a93a5ca4f70a16831518a8aec8d75d6
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-26 17:02:55 +0300

    Update task_repository.py

--- repository/task_repository.py
+++ repository/task_repository.py
@@ -7,16 +7,25 @@ class TaskRepository(AbstractRepository):
     def get_all(self):
         return Task.query.filter_by(is_locked=False).all()
 
-    def get_by_id(self, project_id):
-        return Task.query.filter_by(id=project_id, is_locked=False).first()
+    def get_all_tasks_by_project(self, project_id):
+        return Task.query.filter_by(project_id=project_id, is_locked=False).all()
 
-    def create(self, project):
-        bd.session.add(project)
+    def get_all_tasks_by_project(self, project_id):
+        return Task.query.filter_by(project_id=project_id, is_locked=False).all()
+
+    def get_by_id(self, task_id):
+        return Task.query.filter_by(id=task_id, is_locked=False).first()
+
+    def create(self, task):
+        bd.session.add(task)
         bd.session.commit()
 
-    def update(self, project):
+    def update(self, task):
         bd.session.commit()
 
-    def delete(self, project):
-        bd.session.delete(project)
-        bd.session.commit()
+    def delete(self, task):
+        if task:
+            bd.session.delete(task)
+            bd.session.commit()
+            return True
+        return False
