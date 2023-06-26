dfc1442c2f3e76929dd85c56158e6b5ae77d8578
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-26 17:03:31 +0300

    Update job_model.py

--- models/job_model.py
+++ models/job_model.py
@@ -5,3 +5,4 @@ class Job(bd.Model):
     id = bd.Column(bd.Integer, primary_key=True)
     name_position = bd.Column(bd.String(50), nullable=False)
     tariff_rate = bd.Column(bd.Integer, nullable=False)
+    is_locked = bd.Column(bd.Boolean, nullable=False, default=False)
