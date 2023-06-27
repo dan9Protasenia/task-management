8dedeb6403bfdea0c23a426f77a5f154018dfa1f
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-28 01:00:45 +0300

    Update project.py

--- routes/project.py
+++ routes/project.py
@@ -1,11 +1,14 @@
-from datetime import date, datetime
-
 from flask import Blueprint, request, jsonify
 
-from repository.project_repository import ProjectRepository, Project
+from service.project_service import (
+    get_all_projects,
+    create_project,
+    update_project,
+    delete_project,
+    get_project_tasks,
+)
 
 project = Blueprint('project', __name__)
-repository = ProjectRepository()
 
 
 @project.route('/')
@@ -20,99 +23,34 @@ def get_projects():
     end_date_filter = request.args.get('end_date')
     overdue_filter = request.args.get('overdue')
 
-    projects = repository.get_all()
-    project_list = []
+    projects = get_all_projects(name_filter=name_filter, start_date_filter=start_date_filter,
+                                end_date_filter=end_date_filter, overdue_filter=overdue_filter)
 
-    for project in projects:
-        if name_filter and name_filter.lower() not in project.name.lower():
-            continue
-
-        if start_date_filter:
-            try:
-                start_date_filter = datetime.strptime(start_date_filter, '%Y-%m-%d').date()
-            except ValueError:
-                start_date_filter = None
-            if start_date_filter and project.start_date < start_date_filter:
-                continue
-
-        if end_date_filter:
-            try:
-                end_date_filter = datetime.strptime(end_date_filter, '%Y-%m-%d').date()
-            except ValueError:
-                end_date_filter = None
-            if end_date_filter and project.planned_end_date > end_date_filter:
-                continue
-
-        if overdue_filter == 'true' and project.planned_end_date >= date.today():
-            continue
-
-        project_data = {
-            'id': project.id,
-            'name': project.name,
-            'short_name': project.short_name,
-            'description': project.description,
-            'start_date': project.start_date.strftime('%Y-%m-%d'),
-            'planned_end_date': project.planned_end_date.strftime('%Y-%m-%d'),
-            'actual_end_date': project.actual_end_date.strftime('%Y-%m-%d') if project.actual_end_date else None,
-            'cost': project.cost,
-            'status': project.status
-        }
-        project_list.append(project_data)
-
-    return jsonify(projects=project_list)
+    return jsonify(projects=projects)
 
 
 @project.route('/create_project', methods=['POST'])
-def create_project():
+def create_project_route():
     data = request.get_json()
-    name = data['name']
-    short_name = data['short_name']
-    description = data['description']
-    start_date = date.today()
-    planned_end_date = datetime.strptime(data['planned_end_date'], '%Y-%m-%d').date()
-
-    actual_end_date = None
-    cost = float(data['cost'])
-    status = data['status']
-
-    if status == 'Завершено':
-        actual_end_date = date.today()
-
-    project = Project(
-        name=name,
-        short_name=short_name,
-        description=description,
-        start_date=start_date,
-        planned_end_date=planned_end_date,
-        actual_end_date=actual_end_date,
-        cost=cost,
-        status=status
-    )
-
-    repository.create(project)
-
+    create_project(data)
     return jsonify(message='Project created successfully')
 
 
 @project.route('/edit_project/<int:project_id>', methods=['PUT'])
 def edit_project(project_id):
-    project = repository.get_by_id(project_id)
     data = request.get_json()
-    project.name = data['name']
-    project.short_name = data['short_name']
-    project.description = data['description']
-    project.cost = float(data['cost'])
-    project.status = data['status']
-    project.planned_end_date = datetime.strptime(data['planned_end_date'], '%Y-%m-%d').date()
-
-    repository.update(project)
-
+    update_project(project_id, data)
     return jsonify(message='Project updated successfully')
 
 
 @project.route('/delete_project/<int:project_id>', methods=['DELETE'])
 def delete_project(project_id):
-    project = repository.get_by_id(project_id)
-    repository.delete(project)
+    project = delete_project(project_id)
+    if project:
+        return jsonify(message='Project deleted successfully')
+    return jsonify(message='Project not found')
 
-    return jsonify(message='Project deleted successfully')
+
+@project.route('/projects/<int:project_id>/tasks', methods=['GET'])
+def get_project_tasks_route(project_id):
+    return jsonify(tasks=get_project_tasks(project_id))
