eb3856582958cd7bd1dffd38ea7584be6d4db849
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-27 21:53:06 +0300

    Update task_repository.py

--- repository/task_repository.py
+++ repository/task_repository.py
@@ -1,14 +1,12 @@
 from models.task_model import Task
 from models.task_model import bd
 from . import AbstractRepository
+from models.employee_model import Employee
 
 
 class TaskRepository(AbstractRepository):
     def get_all(self):
         return Task.query.filter_by(is_locked=False).all()
-
-    def get_all_tasks_by_project(self, project_id):
-        return Task.query.filter_by(project_id=project_id, is_locked=False).all()
 
     def get_all_tasks_by_project(self, project_id):
         return Task.query.filter_by(project_id=project_id, is_locked=False).all()
@@ -20,6 +18,7 @@ class TaskRepository(AbstractRepository):
         bd.session.add(task)
         bd.session.commit()
 
+
     def update(self, task):
         bd.session.commit()
 
@@ -28,4 +27,20 @@ class TaskRepository(AbstractRepository):
             bd.session.delete(task)
             bd.session.commit()
             return True
+        return False
+
+    def get_task_performers(self, task_id):
+        task = self.get_by_id(task_id)
+        if task:
+            return task.performers
+        return []
+
+    def set_task_performers(self, task_id, performer_ids):
+        task = self.get_by_id(task_id)
+        if task:
+            performers = Employee.query.filter(Employee.id.in_(performer_ids)).all()
+            if performers:
+                task.performers = performers
+                bd.session.commit()
+                return True
         return False
