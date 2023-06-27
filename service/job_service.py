cfa0cd764ae6d806148f4a5e5089c9ab8ea9080c
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-27 21:52:16 +0300

    Update job_service.py

--- service/job_service.py
+++ service/job_service.py
@@ -1,3 +1,4 @@
+from models.employee_model import Employee
 from repository.job_repository import JobRepository, Job
 
 repository = JobRepository()
@@ -9,7 +10,9 @@ def get_all_jobs():
     for job in jobs:
         job_data = {
             'name_position': job.name_position,
-            'tariff_rate': job.tariff_rate
+            'tariff_rate': job.tariff_rate,
+            'assigned_employees': get_assigned_employees(job.id),
+            'id':job.id
         }
         job_list.append(job_data)
     return job_list
@@ -25,7 +28,7 @@ def create_job(data):
     )
 
     repository.create(job)
-    return [job]
+    return job
 
 
 def update_job(job_id, data):
@@ -43,3 +46,33 @@ def delete_job(job_id):
         repository.delete(job)
         return True
     return False
+
+
+def get_assigned_employees(job_id):
+    employees = Employee.query.filter(Employee.position == job_id).all()
+    employee_list = []
+    for employee in employees:
+        employee_data = {
+            'last_name': employee.last_name,
+            'first_name': employee.first_name,
+            'middle_name': employee.middle_name,
+            'position': employee.position,
+            'id': employee.id
+        }
+        employee_list.append(employee_data)
+    return employee_list
+
+
+def get_employees_by_job(job_id):
+    employees = repository.get_employees(job_id)
+    employee_list = []
+    for employee in employees:
+        employee_data = {
+            'last_name': employee.last_name,
+            'first_name': employee.first_name,
+            'middle_name': employee.middle_name,
+            'position': employee.position,
+            'id': employee.id
+        }
+        employee_list.append(employee_data)
+    return employee_list
