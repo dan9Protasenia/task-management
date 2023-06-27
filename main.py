5c97a1c8bbc3f151736ec71eec9dacf4168ce34a
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-28 00:59:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
-import sqlalchemy
+
 from flask import Flask
 from flask_migrate import Migrate
 from sqlalchemy_utils import create_database
+
 from models.project_model import bd
 from routes.employee import employee
 from routes.job import job
@@ -11,14 +12,11 @@ from routes.task import task
 
 app = Flask(__name__)
 
-
 database_uri = 'sqlite:///C:/Users/Komp/PycharmProjects/qulix/instance/database.db'
 
-# Создание новой базы данных
 create_database(database_uri)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
-  # Опционально, для отключения предупреждений
 
 bd.init_app(app)
 
