f33852cc486a743fc748ea34e41d88d30e9d5ac5
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-06-29 23:37:17 +0300

    Update job_model.py

--- models/job_model.py
+++ models/job_model.py
@@ -2,8 +2,8 @@ from models import bd
 
 
 class Job(bd.Model):
+    __tablename__ = 'job'
     id = bd.Column(bd.Integer, primary_key=True)
     name_position = bd.Column(bd.String(50), nullable=False)
     tariff_rate = bd.Column(bd.Integer, nullable=False)
     is_locked = bd.Column(bd.Boolean, nullable=False, default=False)
-    employees = bd.relationship('Employee', backref='job', lazy=True)
