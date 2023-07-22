97b83730e023bf6b1eafcc28441a8d8531c47ed3
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-07-22 19:51:51 +0300

    Update job_service.py

--- service/job_service.py
+++ service/job_service.py
@@ -1,21 +1,34 @@
 from repository.job_repository import JobRepository, Job
-from service.employee_service import get_assigned_employees
 
 repository = JobRepository()
 
 
-def get_all_jobs():
+def get_job(job_id):
+    job = repository.get_by_id(job_id)
+    if job:
+        job_dict = job_to_dict(job)
+        job_dict['id'] = job.id
+        return job_dict
+    return None
+
+
+def get_all_jobs(name_filter=None):
     jobs = repository.get_all()
     job_list = []
+
     for job in jobs:
-        job_data = {
-            'name_position': job.name_position,
-            'tariff_rate': job.tariff_rate,
-            'assigned_employees': get_assigned_employees(job.id),
-            'id': job.id
-        }
-        job_list.append(job_data)
+        job_dict = job_to_dict(job)
+
+        if name_filter is not None and isinstance(name_filter, str):
+            if name_filter.lower() not in job_dict['name_position'].lower() \
+                    and name_filter.lower() not in job_dict['tariff_rate'].lower():
+                continue
+
+        job_dict['id'] = job.id
+        job_list.append(job_dict)
+
     return job_list
+
 
 
 def create_job(data):
@@ -28,7 +41,7 @@ def create_job(data):
     )
 
     repository.create(job)
-    job.assigned_employees = get_assigned_employees(job.id)
+    # job.assigned_employees = get_assigned_employees(job.id)
     return job
 
 
@@ -48,3 +61,11 @@ def delete_job(job_id):
     return False
 
 
+def job_to_dict(job):
+    job_dict = {
+        'name_position': job.name_position,
+        'tariff_rate': job.tariff_rate,
+        # 'assigned_employees': job.assigned_employees,
+        # Add other fields as needed
+    }
+    return job_dict
