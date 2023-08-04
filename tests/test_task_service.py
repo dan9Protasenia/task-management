740a06e116789e095fb89ce1c87d52c53f39f44e
dan9Protasenia <100715839+dan9Protasenia@users.noreply.github.com>
2023-08-04 20:17:36 +0300

    Update test_task_service.py

--- tests/test_task_service.py
+++ tests/test_task_service.py
@@ -1,10 +1,9 @@
 import unittest
-from datetime import date
+from datetime import date, datetime
 
 from constants import (
     STATUS_IN_PROGRESS,
     STATUS_COMPLETED,
-    TASK_NEW,
     TASK_UPDATED,
     PRIORITY_HIGH,
     PRIORITY_MEDIUM,
@@ -61,31 +60,47 @@ class TaskServiceTestCase(unittest.TestCase):
             status=STATUS_IN_PROGRESS,
             project_id='3'
         )
+
         self.repository.create(task1)
         self.repository.create(task2)
         self.repository.create(task3)
 
-        tasks = get_all_tasks()
+        tasks_project1 = get_all_tasks(task1.project_id)
+        tasks_project2 = get_all_tasks(task2.project_id)
+        tasks_project3 = get_all_tasks(task3.project_id)
 
-        self.assertEqual(len(tasks), 3)
-        self.assertEqual(tasks[0]['name'], TASK1)
-        self.assertEqual(tasks[0]['priority'], PRIORITY_HIGH)
-        self.assertEqual(tasks[0]['start_date'], date.today().strftime('%Y-%m-%d'))
-        self.assertEqual(tasks[0]['planned_end_date'], date.today().strftime('%Y-%m-%d'))
-        self.assertIsNone(tasks[0]['actual_end_date'])
-        self.assertEqual(tasks[0]['status'], STATUS_IN_PROGRESS)
+        self.assertEqual(len(tasks_project1), 1)
+        self.assertEqual(tasks_project1[0]['name'], TASK1)
+        self.assertEqual(tasks_project1[0]['priority'], PRIORITY_HIGH)
+        self.assertEqual(tasks_project1[0]['start_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertEqual(tasks_project1[0]['planned_end_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertIsNone(tasks_project1[0]['actual_end_date'])
+        self.assertEqual(tasks_project1[0]['status'], STATUS_IN_PROGRESS)
 
+        self.assertEqual(len(tasks_project2), 1)
+        self.assertEqual(tasks_project2[0]['name'], TASK2)
+        self.assertEqual(tasks_project2[0]['priority'], PRIORITY_MEDIUM)
+        self.assertEqual(tasks_project2[0]['start_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertEqual(tasks_project2[0]['planned_end_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertIsNone(tasks_project2[0]['actual_end_date'])
+        self.assertEqual(tasks_project2[0]['status'], STATUS_IN_PROGRESS)
+
+        self.assertEqual(len(tasks_project3), 1)
+        self.assertEqual(tasks_project3[0]['name'], TASK3)
+        self.assertEqual(tasks_project3[0]['priority'], PRIORITY_LOW)
+        self.assertEqual(tasks_project3[0]['start_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertEqual(tasks_project3[0]['planned_end_date'], date.today().strftime('%Y-%m-%d'))
+        self.assertIsNone(tasks_project3[0]['actual_end_date'])
+        self.assertEqual(tasks_project3[0]['status'], STATUS_IN_PROGRESS)
 
     def test_create_task(self):
         project_id = 1
         data = {
             'name': 'New Task',
             'priority': 'High',
-            'status': 'In Progress'
-
+            'status': 'In Progress',
+            'planned_end_date': '2023-07-20'
         }
-
-
 
         create_task(project_id, data)
         task = self.repository.get_all()[0]
@@ -94,11 +109,10 @@ class TaskServiceTestCase(unittest.TestCase):
         self.assertEqual(task.name, 'New Task')
         self.assertEqual(task.priority, 'High')
         self.assertEqual(task.start_date, date.today())
-        self.assertEqual(task.planned_end_date, date.today())
+        self.assertEqual(task.planned_end_date, datetime.strptime(data['planned_end_date'], '%Y-%m-%d').date())
         self.assertIsNone(task.actual_end_date)
         self.assertEqual(task.status, 'In Progress')
         self.assertEqual(task.project_id, 1)
-
 
     def test_update_task(self):
         task = Task(
@@ -142,76 +156,20 @@ class TaskServiceTestCase(unittest.TestCase):
 
         self.assertIsNone(deleted_task)
 
-    def test_get_all_tasks_with_filters(self):
-        task1 = Task(
-            name=TASK1,
-            priority=PRIORITY_HIGH,
-            start_date=date(2023, 1, 1),
-            planned_end_date=date(2023, 1, 5),
-            actual_end_date=None,
-            status=STATUS_IN_PROGRESS,
-            project_id='1'
-        )
-        task2 = Task(
-            name=TASK2,
-            priority=PRIORITY_MEDIUM,
-            start_date=date(2023, 1, 2),
-            planned_end_date=date(2023, 1, 7),
-            actual_end_date=None,
-            status=STATUS_IN_PROGRESS,
-            project_id='1'
-        )
-        task3 = Task(
-            name=TASK3,
-            priority=PRIORITY_LOW,
-            start_date=date(2023, 1, 3),
-            planned_end_date=date(2023, 1, 9),
-            actual_end_date=None,
-            status=STATUS_COMPLETED,
-            project_id='1'
-        )
-        self.repository.create(task1)
-        self.repository.create(task2)
-        self.repository.create(task3)
 
-        # Test filtering by name
-        response = self.client.get('/tasks/1?name=Task 1')
-        data = response.get_json()
-        self.assertEqual(len(data['tasks']), 1)
-        self.assertEqual(data['tasks'][0]['name'], TASK1)
+# def setUp(self):
+#     app.testing = True
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#     self.app_context = app.app_context()
+#     self.app_context.push()
+#     bd.create_all()
+#     self.client = app.test_client()
+#     self.repository = TaskRepository()
 
-        # Test filtering by start date
-        response = self.client.get('/tasks/1?start_date_from=2023-01-02&start_date_to=2023-01-04')
-        data = response.get_json()
-        self.assertEqual(len(data['tasks']), 2)
-        self.assertEqual(data['tasks'][0]['name'], TASK1)
-        self.assertEqual(data['tasks'][1]['name'], TASK2)
-
-        response = self.client.get('/tasks/1?end_date_from=2023-01-06&end_date_to=2023-01-08')
-        data = response.get_json()
-        self.assertEqual(len(data['tasks']), 1)
-        self.assertEqual(data['tasks'][0]['name'], TASK2)
-
-        response = self.client.get('/tasks/1?status=Completed')
-        data = response.get_json()
-        self.assertEqual(len(data['tasks']), 1)
-        self.assertEqual(data['tasks'][0]['name'], TASK3)
-
-
-def setUp(self):
-    app.testing = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    self.app_context = app.app_context()
-    self.app_context.push()
-    bd.create_all()
-    self.client = app.test_client()
-    self.repository = TaskRepository()
-
-
-def tearDown(self):
-    bd.session.remove()
-    bd.drop_all()
-
+#
+# def tearDown(self):
+#     bd.session.remove()
+#     bd.drop_all()
 
 
 if __name__ == '__main__':
